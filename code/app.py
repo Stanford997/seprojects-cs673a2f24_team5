95d5e9ea85baa453bafb20cd9a7d77487108cb38
Stanford997 <545255309@qq.com>
2024-10-08 22:52:00 -0400

    feat(configuration): add config.json

--- code/app.py
+++ code/app.py
@@ -1,15 +1,18 @@
+import json
+import os
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from configuration.config import Config
 from modules.upload import upload_parse_resume
 
 app = Flask(__name__)
 
-app.config.from_object(Config)
+config_path = os.path.join('configs', 'config.json')
+with open(config_path, 'r') as file:
+    config = json.load(file)
 
 # MongoDB
-client = MongoClient(app.config['MONGO_URI'])
+client = MongoClient(config['MONGO_URI'])
 
 db = client['resume_db']
 resume_collection = db['resumes']
@@ -20,8 +23,10 @@ def upload_resume():
     user_id = request.form.get('user_id')
     if not user_id:
         return jsonify({"error": "No user ID provided."}), 400
-
+    # vector the resume text
     return upload_parse_resume(request, resume_collection)
+
+# def evaluation():
 
 
 if __name__ == '__main__':
--- code/configuration/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    MONGO_URI = "Your MongoDB URI"
-    UPLOAD_FOLDER = 'uploads/'
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Max pdf size 16 MB
