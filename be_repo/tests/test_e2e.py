54b09c3a6bcee49482073f1fde5eb954a4bbefed
Stanford997 <545255309@qq.com>
2024-11-06 20:36:59 -0500

    test

--- be_repo/tests/test_e2e.py
+++ be_repo/tests/test_e2e.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
