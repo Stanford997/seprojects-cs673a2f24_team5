a35c10ecdb16a86be0360ba2fba69beed7ef6fed
Stanford997 <545255309@qq.com>
2024-11-06 21:34:37 -0500

    test

--- be_repo/tests/test_e2e.py
+++ be_repo/tests/test_e2e.py
@@ -10,45 +10,17 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))
-display.start()
-FRONTEND_URL = "http://localhost:3001"
 
-# chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-#
-# chrome_options = Options()
-# options = [
-#     "--headless",
-#     "--disable-gpu",
-#     "--window-size=1920,1200",
-#     "--ignore-certificate-errors",
-#     "--disable-extensions",
-#     "--no-sandbox",
-#     "--disable-dev-shm-usage"
-# ]
-# for option in options:
-#     chrome_options.add_argument(option)
-#
-# driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+FRONTEND_URL = "http://localhost:3001"
 
 chromedriver_autoinstaller.install()
 
 chrome_options = webdriver.ChromeOptions()
 # Add your options as needed
 options = [
-    # Define window size here
     "--window-size=1200,1200",
     "--ignore-certificate-errors"
-
-    # "--headless",
-    # "--disable-gpu",
-    # "--window-size=1920,1200",
-    # "--ignore-certificate-errors",
-    # "--disable-extensions",
-    # "--no-sandbox",
-    # "--disable-dev-shm-usage",
-    # '--remote-debugging-port=9222'
+    "--headless",
 ]
 
 for option in options:
