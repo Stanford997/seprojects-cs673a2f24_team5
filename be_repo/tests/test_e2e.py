78b02b5b4e5ed923cccaae7e9b00b0b2b2f47c91
Stanford997 <545255309@qq.com>
2024-11-06 21:13:40 -0500

    test

--- be_repo/tests/test_e2e.py
+++ be_repo/tests/test_e2e.py
@@ -29,28 +29,29 @@ FRONTEND_URL = "http://localhost:3001"
 #
 # driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-# and if it doesn't exist, download it automatically,
-# then add chromedriver to path
+chromedriver_autoinstaller.install()
 
 chrome_options = webdriver.ChromeOptions()
 # Add your options as needed
 options = [
+    # Define window size here
+    "--window-size=1200,1200",
     "--ignore-certificate-errors"
-    "--headless",
-    "--disable-gpu",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage",
+
+    # "--headless",
+    # "--disable-gpu",
+    # "--window-size=1920,1200",
+    # "--ignore-certificate-errors",
+    # "--disable-extensions",
+    # "--no-sandbox",
+    # "--disable-dev-shm-usage",
+    # '--remote-debugging-port=9222'
 ]
 
 for option in options:
     chrome_options.add_argument(option)
 
 driver = webdriver.Chrome(options=chrome_options)
-
-
 
 driver.get(FRONTEND_URL)
 wait = WebDriverWait(driver, 10)
